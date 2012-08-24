bc11a27631a02996a333f897e8c7a41d6fe03dec
jqvar <jan.qvarnstrom@su.se>
2012-08-24 08:40:24 +0200

    Groovyfying

--- src/main/groovy/se/su/it/svc/audit/AuditAspect.groovy
+++ src/main/groovy/se/su/it/svc/audit/AuditAspect.groovy
@@ -17,72 +17,72 @@ import se.su.it.svc.commons.SvcAudit
 
 public class AuditAspect implements MethodInterceptor {
   private static final Logger logger = Logger.getLogger(AuditAspect.class)
-  private static final String STATE_INPROGRESS = "IN PROGRESS";
-  private static final String STATE_SUCCESS = "SUCCESS";
-  private static final String STATE_EXCEPTION = "EXCEPTION";
-  private static final String UNKNOWN = "<unknown>";
+  private static final String STATE_INPROGRESS = "IN PROGRESS"
+  private static final String STATE_SUCCESS = "SUCCESS"
+  private static final String STATE_EXCEPTION = "EXCEPTION"
+  private static final String UNKNOWN = "<unknown>"
 
   public Object invoke(MethodInvocation invocation) throws Throwable {
 
     Object rval
 
     // Log the invocation attempt, keep a reference to the opaque audit object
-    Object auditRef = null;
+    Object auditRef = null
     try {
-      auditRef = logBefore(invocation.getMethod(), invocation.getArguments());
+      auditRef = logBefore(invocation.getMethod(), invocation.getArguments())
     } catch (Exception e) {
-      logger.warn("logBefore failed for " + invocation.getMethod().getName() + " - Not proceeding.");
-      throw new Exception("Audit Engine is DOWN - not performing any actions at the moment", e);
+      logger.warn("logBefore failed for " + invocation.getMethod().getName() + " - Not proceeding.")
+      throw new Exception("Audit Engine is DOWN - not performing any actions at the moment", e)
     }
 
     // Attempt to execute the method
     try {
-      rval = invocation.proceed();
+      rval = invocation.proceed()
     } catch (Throwable e) {
       if (auditRef != null) {
-        logException(auditRef, e);
+        logException(auditRef, e)
       }
-      logger.debug("Method invocation threw exception, re-throwing to caller");
-      throw e;
+      logger.debug("Method invocation threw exception, re-throwing to caller")
+      throw e
     }
 
     // Add the return value to the audit log entry
     if (auditRef != null) {
-      logAfter(auditRef, rval);
+      logAfter(auditRef, rval)
     }
     // Return the method return value to the caller
-    return rval;
+    return rval
   }
 
   protected Object logBefore(Method mi, Object[] args) throws Exception {
     // Surround the audit logging with a global try/catch so that we can do softFail in a single catch block
     try {
-      logger.info("Invoked " + mi.getName() + " with " + args.length + " params");
+      logger.info("Invoked " + mi.getName() + " with " + args.length + " params")
 
       // Serialize the argument Object list into a ByteArray
-      ByteArrayOutputStream bsArgs = new ByteArrayOutputStream();
-      ObjectOutputStream outArgs = new ObjectOutputStream(bsArgs);
-      outArgs.writeObject(args);
-      outArgs.close();
+      ByteArrayOutputStream bsArgs = new ByteArrayOutputStream()
+      ObjectOutputStream outArgs = new ObjectOutputStream(bsArgs)
+      outArgs.writeObject(args)
+      outArgs.close()
 
       // Determine uid and ip to use in the audit entry
-      String auditIp;
-      String auditUid;
-      String auditClient;
+      String auditIp
+      String auditUid
+      String auditClient
 
-      int lastArgIdx = args.length - 1;
+      int lastArgIdx = args.length - 1
       if (args[lastArgIdx] != null && args[lastArgIdx] instanceof SvcAudit) {
-        logger.debug("Found a non-null SvcAudit as last argument to method, extracting uid and ip");
-        SvcAudit audit = (SvcAudit) args[lastArgIdx];
-        auditIp = audit.getIpAddress();
-        auditUid = audit.getUid();
-        auditClient = audit.getClient();
+        logger.debug("Found a non-null SvcAudit as last argument to method, extracting uid and ip")
+        SvcAudit audit = (SvcAudit) args[lastArgIdx]
+        auditIp = audit.getIpAddress()
+        auditUid = audit.getUid()
+        auditClient = audit.getClient()
       } else {
         logger.warn("No suitable SvcAudit supplied for call to " + mi.getName() +
-          " - will not be able to log originator IP/UID.");
-        auditIp = UNKNOWN;
-        auditUid = UNKNOWN;
-        auditClient = UNKNOWN;
+          " - will not be able to log originator IP/UID.")
+        auditIp = UNKNOWN
+        auditUid = UNKNOWN
+        auditClient = UNKNOWN
       }
 
       // Create an AuditEntity based on the gathered information
@@ -100,26 +100,26 @@ public class AuditAspect implements MethodInterceptor {
       //TODO: Call RabbitMQ here to transmit the "audit before data"
 
       // Return a reference to the ae for reuse in success/exception loggers
-      return ae;
+      return ae
 
     } catch (Exception e) {
-        logger.warn("Audit logging failed, catching exception due to SoftFail-mode being set to true", e);
-        return null;
+        logger.warn("Audit logging failed, catching exception due to SoftFail-mode being set to true", e)
+        return null
     }
   }
 
   protected void logAfter(Object ref, Object ret) {
 
     try {
-      String ae = (String) ref;
+      String ae = (String) ref
 
-      logger.info("Decorating ae " + ae + " with return value: " + ret);
+      logger.info("Decorating ae " + ae + " with return value: " + ret)
 
       // Serialize the Return object into a ByteArray
-      ByteArrayOutputStream bsRet = new ByteArrayOutputStream();
-      ObjectOutputStream outRet = new ObjectOutputStream(bsRet);
-      outRet.writeObject(ret);
-      outRet.close();
+      ByteArrayOutputStream bsRet = new ByteArrayOutputStream()
+      ObjectOutputStream outRet = new ObjectOutputStream(bsRet)
+      outRet.writeObject(ret)
+      outRet.close()
 
       // Append return value to the audit entity
       ae += "Text_return: " + objectToString(ret) + "\r\n"
@@ -129,17 +129,17 @@ public class AuditAspect implements MethodInterceptor {
       //TODO: Call RabbitMQ here to transmit the "audit after data"
 
     } catch (Exception e) {
-      logger.warn("Audit logging failed, no return value will be stored", e);
+      logger.warn("Audit logging failed, no return value will be stored", e)
     }
   }
 
   protected void logException(Object ref, Throwable t) {
 
     try {
-      String ae = (String) ref;
+      String ae = (String) ref
 
       // TBD: Check for cast exception and nullity of ref
-      logger.info("Decorating ae " + ae + " with exception " + t.getClass().getCanonicalName());
+      logger.info("Decorating ae " + ae + " with exception " + t.getClass().getCanonicalName())
 
       // Append return value to the audit entity
       ae += "Text_return: " + t.toString() + "\r\n"
@@ -148,30 +148,30 @@ public class AuditAspect implements MethodInterceptor {
       //TODO: Call RabbitMQ here to transmit the "audit after data"
 
     } catch (Exception e) {
-      logger.warn("Audit logging of thrown exception failed", e);
+      logger.warn("Audit logging of thrown exception failed", e)
     }
   }
 
   protected String objectToString(Object o) {
     if (o == null)
-      return "null";
+      return "null"
 
-    return o.toString();
+    return o.toString()
   }
 
   protected String objectToString(Object[] o) {
     if (o == null)
-      return "null";
+      return "null"
 
-    StringBuilder strBuilder = new StringBuilder();
-    strBuilder.append("[");
+    StringBuilder strBuilder = new StringBuilder()
+    strBuilder.append("[")
     for (Object obj : o) {
-      strBuilder.append(objectToString(obj));
-      strBuilder.append(",");
+      strBuilder.append(objectToString(obj))
+      strBuilder.append(",")
     }
-    strBuilder.deleteCharAt(strBuilder.length() - 1);
-    strBuilder.append("]");
-    return strBuilder.toString();
+    strBuilder.deleteCharAt(strBuilder.length() - 1)
+    strBuilder.append("]")
+    return strBuilder.toString()
 
   }
 }
