b07ecc57df036fd413244b0d97d3063e119597be
Jack <jack.enqvist@su.se>
2012-08-29 13:29:55 +0200

    javadoc for se.su.it.svc.manager.Properties

--- src/main/groovy/se/su/it/svc/manager/Properties.groovy
+++ src/main/groovy/se/su/it/svc/manager/Properties.groovy
@@ -3,6 +3,17 @@ package se.su.it.svc.manager
 /**
  * Created by: Jack Enqvist (jaen4109)
  * Date: 2012-08-28 ~ 09:13
+ *
+ * this Singleton fetches a properties-fle from the filesystem and provides it as a groovy.util.ConfigObject.
+ * the properties can then be accessed from the singleton objects field props.
+ *
+ * example:
+ * def props = Properties.getInstance().props
+ *
+ * def databaseUser     = props.database.user
+ * def databasePassword = props.database.password
+ * def databaseServer   = props.database.serverURL
+ *
  */
 class Properties {
   private static final Properties INSTANCE = new Properties()
