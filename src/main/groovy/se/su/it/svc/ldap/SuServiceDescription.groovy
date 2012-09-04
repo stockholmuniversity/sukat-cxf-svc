dee3075617e5a26c46200d9aae064381c5b173b3
jqvar <jan.qvarnstrom@su.se>
2012-09-04 12:09:55 +0200

    Added annotations for jaxb

--- src/main/groovy/se/su/it/svc/ldap/SuServiceDescription.groovy
+++ src/main/groovy/se/su/it/svc/ldap/SuServiceDescription.groovy
@@ -2,10 +2,16 @@ package se.su.it.svc.ldap
 
 import gldapo.schema.annotation.GldapoSchemaFilter
 import gldapo.schema.annotation.GldapoNamingAttribute
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlRootElement
+import javax.xml.bind.annotation.XmlAttribute
 
 /**
  * GLDAPO schema class for holding SU Service Description also used by web service.
  */
+@XmlAccessorType( XmlAccessType.NONE )
+@XmlRootElement
 class SuServiceDescription implements Serializable {
 
   static final long serialVersionUID = -687991492884005233L;
@@ -13,10 +19,16 @@ class SuServiceDescription implements Serializable {
   @GldapoSchemaFilter("(objectClass=suServiceDescription)")
 
   @GldapoNamingAttribute
+  @XmlAttribute
   String cn
+  @XmlAttribute
   String suServiceType
+  @XmlAttribute
   String suServicePolicy
+  @XmlAttribute
   String suServiceURI
+  @XmlAttribute
   String description
+  @XmlAttribute
   String displayName
 }
