9c33d50db0cc157ae7789bd47aaa5326ec04bef4
jqvar <jan.qvarnstrom@su.se>
2012-09-11 13:21:59 +0200

    Javadoc for updateAffiliation

--- src/main/groovy/se/su/it/svc/AccountServiceImpl.groovy
+++ src/main/groovy/se/su/it/svc/AccountServiceImpl.groovy
@@ -14,6 +14,17 @@ import javax.jws.WebParam
 public class AccountServiceImpl implements AccountService{
   private static final Logger logger = Logger.getLogger(AccountServiceImpl.class)
 
+  /**
+   * This method sets the primary affiliation for the specified uid.
+   *
+   *
+   * @param uid  uid of the user.
+   * @param affiliation the affiliation for this uid
+   * @param audit Audit object initilized with audit data about the client and user.
+   * @return array of SuService.
+   * @see se.su.it.svc.ldap.SuPerson
+   * @see se.su.it.svc.commons.SvcAudit
+   */
   public void updateAffiliation(@WebParam(name = "uid") String uid, @WebParam(name = "affiliation") String affiliation, @WebParam(name = "audit") SvcAudit audit) {
     if(uid == null || affiliation == null || audit == null)
       throw new java.lang.IllegalArgumentException("updateAffiliation - Null argument values not allowed in this function")
