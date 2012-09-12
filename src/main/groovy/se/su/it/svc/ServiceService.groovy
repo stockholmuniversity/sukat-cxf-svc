268a30e339f40ccaca8cd550fa46d0649b0e8c4e
jqvar <jan.qvarnstrom@su.se>
2012-09-12 14:22:13 +0200

    Added auditaspectmethoddetails annotation for serviceservice web service method enableServiceFully

--- src/main/groovy/se/su/it/svc/ServiceService.groovy
+++ src/main/groovy/se/su/it/svc/ServiceService.groovy
@@ -4,11 +4,13 @@ import se.su.it.svc.annotations.SuCxfSvcSpocpRole
 import se.su.it.svc.commons.SvcAudit
 import se.su.it.svc.ldap.SuService
 import se.su.it.svc.ldap.SuServiceDescription
+import se.su.it.svc.audit.AuditAspectMethodDetails
 
 @SuCxfSvcSpocpRole(role = "sukat-service-admin")
 public interface ServiceService {
   public SuService[] getServices(String uid, SvcAudit audit)
   public SuServiceDescription[] getServiceTemplates(SvcAudit audit)
+  @AuditAspectMethodDetails(details = "resetOrCreatePrincipal")
   public SuService enableServiceFully(String uid, String serviceType, String qualifier, String description, SvcAudit audit)
   public void blockService(String uid, String serviceType, SvcAudit audit)
   public void unblockService(String uid, String serviceType, SvcAudit audit)
