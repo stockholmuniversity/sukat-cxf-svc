ba2883fea207f2085de632193e1884c4cc203dc2
jqvar <jan.qvarnstrom@su.se>
2012-09-12 15:12:28 +0200

    Added spocp role for enrollment service web service

--- src/main/groovy/se/su/it/svc/EnrollmentService.groovy
+++ src/main/groovy/se/su/it/svc/EnrollmentService.groovy
@@ -1,11 +1,13 @@
 package se.su.it.svc
 
 import se.su.it.svc.commons.SvcAudit
+import se.su.it.svc.annotations.SuCxfSvcSpocpRole
 
 /**
  * Created by: Jack Enqvist (jaen4109)
  * Date: 2012-09-06 ~ 09:38
  */
+@SuCxfSvcSpocpRole(role = "sukat-user-admin")
 public interface EnrollmentService {
   public String enrollUserByUid(java.lang.String uid, SvcAudit audit)
   void expireUserPassword(java.lang.String uid, SvcAudit audit)
