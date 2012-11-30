69697f0873d2feb3a27caf1810e560bc38282458
jqvar <jan.qvarnstrom@su.se>
2012-11-30 11:39:29 +0100

    Fixed default configuration in beans-groovy.xml and in our Properties.groovy class

--- src/main/groovy/se/su/it/svc/manager/Properties.groovy
+++ src/main/groovy/se/su/it/svc/manager/Properties.groovy
@@ -1,5 +1,7 @@
 package se.su.it.svc.manager
 
+import org.apache.log4j.Logger
+
 /**
  * Created by: Jack Enqvist (jaen4109)
  * Date: 2012-08-28 ~ 09:13
@@ -16,7 +18,9 @@ package se.su.it.svc.manager
  *
  */
 class Properties {
+  private static final Logger logger = Logger.getLogger(se.su.it.svc.manager.Properties.class)
   private static final Properties INSTANCE = new Properties()
+  private static final String CONFFILE = "/local/cxf-server/conf/config.properties"
 
   public ConfigObject props
 
@@ -38,8 +42,39 @@ class Properties {
    */
   private loadProperties(){
     def props = new java.util.Properties()
-    new File("/local/cxf-server/conf/config.properties").withInputStream { stream ->
-      props.load(stream)
+    //Begin Default Values
+    //Database
+    props.put("database.url", "jdbc:mysql://localhost/gormtest")
+    props.put("database.driver", "com.mysql.jdbc.Driver")
+    props.put("database.user", "gormtest")
+    props.put("database.password", "gormtest")
+    //Ldap
+    props.put("ldap.serverro", "ldap://ldap-test.su.se")
+    props.put("ldap.serverrw", "ldap://sukat-test-ldaprw02.it.su.se")
+    //Ssl
+    props.put("ssl.port", 443)
+    props.put("ssl.keystore", "cxf-svc-server.keystore")
+    props.put("ssl.password", "changeit")
+    //Spnego
+    props.put("spnego.properties", "spnego.properties")
+    props.put("spnego.realm", "SU.SE")
+    //Ehcache
+    props.put("ehcache.maxElementsInMemory", 10000)
+    props.put("ehcache.eternal", false)
+    props.put("ehcache.timeToIdleSeconds", 120)
+    props.put("ehcache.timeToLiveSeconds", 600)
+    props.put("ehcache.overflowToDisk", false)
+    props.put("ehcache.diskPersistent", false)
+    props.put("ehcache.diskExpiryThreadIntervalSeconds", 120)
+    props.put("ehcache.memoryStoreEvictionPolicy", "LRU")
+    //End Default Values
+    try {
+      new File(CONFFILE).withInputStream { stream ->
+        props.load(stream)
+      }
+    } catch (Exception e) {
+      logger.warn("Exception when trying to read configuration file " + CONFFILE + ", exception message was: " + e.message + ".")
+      logger.warn("Using default properties for application!")
     }
     this.props = new ConfigSlurper().parse(props)
   }
