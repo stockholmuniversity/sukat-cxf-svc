28b900d23828d25d1ec41bdcc432c28028a53ae6
jqvar <jan.qvarnstrom@su.se>
2012-12-07 11:15:10 +0100

    Config file handling change to a flow wished by linfra. Check if we have a system property called config.properties if found use this file to read configuration from. Else use default properties. The server will not start if system property was found but not the file defined by system property or missing mandatory properties in file. Also bumped the version.

--- src/main/groovy/se/su/it/svc/manager/Properties.groovy
+++ src/main/groovy/se/su/it/svc/manager/Properties.groovy
@@ -20,11 +20,10 @@ import org.apache.log4j.Logger
 class Properties {
   private static final Logger logger = Logger.getLogger(se.su.it.svc.manager.Properties.class)
   private static final Properties INSTANCE = new Properties()
-  private static final String CONFFILE = "/local/cxf-server/conf/config.properties"
 
   public ConfigObject props
 
-  private Properties(){
+  private Properties() {
     loadProperties()
   }
 
@@ -32,7 +31,7 @@ class Properties {
    *
    * @return the singleton instance containing a groovy.util.ConfigObject called props.
    */
-  static getInstance(){
+  static getInstance() {
     return INSTANCE
   }
 
@@ -40,47 +39,49 @@ class Properties {
    * loading the properties file from the filesystem at a predefined path.
    * populates the public member field props with a groovy.util.ConfigObject containing the properties
    */
-  private loadProperties(){
+  private loadProperties() {
     def props = new java.util.Properties()
-    //Begin Default Values
-    //Database
-    props.put("database.url", "jdbc:mysql://localhost/gormtest")
-    props.put("database.driver", "com.mysql.jdbc.Driver")
-    props.put("database.user", "gormtest")
-    props.put("database.password", "gormtest")
-    //Ldap
-    props.put("ldap.serverro", "ldap://ldap-test.su.se")
-    props.put("ldap.serverrw", "ldap://sukat-test-ldaprw02.it.su.se")
-    //Ssl
-    props.put("http.port", 443)
-    props.put("ssl.enabled", true);
-    props.put("ssl.keystore", "cxf-svc-server.keystore")
-    props.put("ssl.password", "changeit")
-    //Spnego
-    props.put("spnego.properties", "spnego.properties")
-    props.put("spnego.realm", "SU.SE")
-    //Ehcache
-    props.put("ehcache.maxElementsInMemory", 10000)
-    props.put("ehcache.eternal", false)
-    props.put("ehcache.timeToIdleSeconds", 120)
-    props.put("ehcache.timeToLiveSeconds", 600)
-    props.put("ehcache.overflowToDisk", false)
-    props.put("ehcache.diskPersistent", false)
-    props.put("ehcache.diskExpiryThreadIntervalSeconds", 120)
-    props.put("ehcache.memoryStoreEvictionPolicy", "LRU")
-    //End Default Values
-    try {
-      new File(CONFFILE).withInputStream { stream ->
-        props.load(stream)
+    String definedConfigFileName = System.getProperty("config.properties")
+    if (definedConfigFileName != null) {
+      try {
+        new File(definedConfigFileName.trim()).withInputStream { stream ->
+          props.load(stream)
+        }
+      } catch (Exception e) {
+        logger.error("Exception when trying to read configuration file " + definedConfigFileName.trim() + ", exception message was: " + e.message + ".")
+        logger.error("This instance will be highly unstable!")
       }
-    } catch (Exception e) {
-      logger.warn("Exception when trying to read configuration file " + CONFFILE + ", exception message was: " + e.message + ".")
-      logger.warn("Using default properties for application!")
+    } else {
+      //Begin Default Values
+      //Database
+      props.put("database.url", "jdbc:mysql://localhost/gormtest")
+      props.put("database.driver", "com.mysql.jdbc.Driver")
+      props.put("database.user", "gormtest")
+      props.put("database.password", "gormtest")
+      //Ldap
+      props.put("ldap.serverro", "ldap://ldap-test.su.se")
+      props.put("ldap.serverrw", "ldap://sukat-test-ldaprw02.it.su.se")
+      //Ssl
+      props.put("http.port", 443)
+      props.put("ssl.enabled", true);
+      props.put("ssl.keystore", "cxf-svc-server.keystore")
+      props.put("ssl.password", "changeit")
+      //Spnego
+      props.put("spnego.properties", "spnego.properties")
+      props.put("spnego.realm", "SU.SE")
+      //Ehcache
+      props.put("ehcache.maxElementsInMemory", 10000)
+      props.put("ehcache.eternal", false)
+      props.put("ehcache.timeToIdleSeconds", 120)
+      props.put("ehcache.timeToLiveSeconds", 600)
+      props.put("ehcache.overflowToDisk", false)
+      props.put("ehcache.diskPersistent", false)
+      props.put("ehcache.diskExpiryThreadIntervalSeconds", 120)
+      props.put("ehcache.memoryStoreEvictionPolicy", "LRU")
+      //End Default Values
     }
     this.props = new ConfigSlurper().parse(props)
   }
-
-
 
 
 }
