ee72bfc566da3dcee08a77fdf9dcc94e0b13c4f3
jqvar <jan.qvarnstrom@su.se>
2012-12-14 13:18:31 +0100

    Added shared memory bridge between container and webbapp for setting loglevel on container loggers.

--- src/main/groovy/se/su/it/svc/WebServiceAdminImpl.groovy
+++ src/main/groovy/se/su/it/svc/WebServiceAdminImpl.groovy
@@ -7,28 +7,36 @@ import org.apache.log4j.LogManager
 import se.su.it.svc.util.WebServiceAdminUtils
 import javax.jws.WebParam
 import se.su.it.svc.commons.SvcAudit
+import java.nio.channels.FileChannel
+import java.nio.MappedByteBuffer
 
 @WebService
 public class WebServiceAdminImpl implements WebServiceAdmin{
   private static final Logger logger = Logger.getLogger(WebServiceAdminImpl.class)
+  private static final MappedByteBuffer mem = new RandomAccessFile("/tmp/cxf-server-tmp.txt", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, 1);
 
   void setRootLogLevel(@WebParam(name = "level") String level, @WebParam(name = "audit") SvcAudit audit) {
     LogManager.getRootLogger().setLevel(WebServiceAdminUtils.getLogLevelFromString(level));
+    mem.put(0, (byte)WebServiceAdminUtils.getFunctionByteFromLoggerAndString("root", level));
   }
 
   void setApplicationLogLevel(@WebParam(name = "level") String level, @WebParam(name = "audit") SvcAudit audit) {
     LogManager.getLogger("se.su.it.svc").setLevel(WebServiceAdminUtils.getLogLevelFromString(level))
+    mem.put(0, (byte)WebServiceAdminUtils.getFunctionByteFromLoggerAndString("app", level));
   }
 
   void setContainerLogLevel(@WebParam(name = "level") String level, @WebParam(name = "audit") SvcAudit audit) {
     LogManager.getLogger("org.eclipse.jetty").setLevel(WebServiceAdminUtils.getLogLevelFromString(level))
+    mem.put(0, (byte)WebServiceAdminUtils.getFunctionByteFromLoggerAndString("jetty", level));
   }
 
   void setCxfLogLevel(@WebParam(name = "level") String level, @WebParam(name = "audit") SvcAudit audit) {
     LogManager.getLogger("org.apache.cxf").setLevel(WebServiceAdminUtils.getLogLevelFromString(level))
+    mem.put(0, (byte)WebServiceAdminUtils.getFunctionByteFromLoggerAndString("cxf", level));
   }
 
   void setSpringLogLevel(@WebParam(name = "level") String level, @WebParam(name = "audit") SvcAudit audit) {
     LogManager.getLogger("org.springframework").setLevel(WebServiceAdminUtils.getLogLevelFromString(level))
+    mem.put(0, (byte)WebServiceAdminUtils.getFunctionByteFromLoggerAndString("spring", level));
   }
 }
--- src/main/groovy/se/su/it/svc/util/WebServiceAdminUtils.groovy
+++ src/main/groovy/se/su/it/svc/util/WebServiceAdminUtils.groovy
@@ -26,5 +26,77 @@ public class WebServiceAdminUtils {
         return Level.INFO
     }
   }
+
+  public static byte getFunctionByteFromLoggerAndString(String logger, String level) {
+    if(logger == null || logger.length() <= 0 || level == null || level.length() <= 0) {
+      logger.info("logger/level <null>! Using default log level INFO for root logger!")
+      return Level.INFO
+    }
+    switch (logger) {
+
+      case "root" : switch (level.toLowerCase()) {
+        case  "all"   : return 1
+        case  "trace" : return 2
+        case  "debug" : return 3
+        case  "info"  : return 4
+        case  "warn"  : return 5
+        case  "fatal" : return 6
+        case  "error" : return 7
+        case  "off"   : return 8
+        default       : logger.info("No such log level <" + level + ">! Using default log level INFO!")
+          return 4
+      }
+      case "app" : switch (level.toLowerCase()) {
+        case  "all"   : return 9
+        case  "trace" : return 10
+        case  "debug" : return 11
+        case  "info"  : return 12
+        case  "warn"  : return 13
+        case  "fatal" : return 14
+        case  "error" : return 15
+        case  "off"   : return 16
+        default       : logger.info("No such log level <" + level + ">! Using default log level INFO!")
+          return 12
+      }
+      case "jetty" : switch (level.toLowerCase()) {
+        case  "all"   : return 17
+        case  "trace" : return 18
+        case  "debug" : return 19
+        case  "info"  : return 20
+        case  "warn"  : return 21
+        case  "fatal" : return 22
+        case  "error" : return 23
+        case  "off"   : return 24
+        default       : logger.info("No such log level <" + level + ">! Using default log level INFO!")
+          return 20
+      }
+      case "spring" : switch (level.toLowerCase()) {
+        case  "all"   : return 25
+        case  "trace" : return 26
+        case  "debug" : return 27
+        case  "info"  : return 28
+        case  "warn"  : return 29
+        case  "fatal" : return 30
+        case  "error" : return 31
+        case  "off"   : return 32
+        default       : logger.info("No such log level <" + level + ">! Using default log level INFO!")
+          return 28
+      }
+      case "cxf" : switch (level.toLowerCase()) {
+        case  "all"   : return 33
+        case  "trace" : return 34
+        case  "debug" : return 35
+        case  "info"  : return 36
+        case  "warn"  : return 37
+        case  "fatal" : return 38
+        case  "error" : return 39
+        case  "off"   : return 40
+        default       : logger.info("No such log level <" + level + ">! Using default log level INFO!")
+          return 36
+      }
+      default : logger.info("No such logger <" + logger + ">! Using default root logger setting level to INFO!")
+        return 4
+    }
+  }
 }
 
