9073f5755925075c838dc2632161ad849a526fdf
Tommy Andersson <tomma@it.su.se>
2013-07-01 09:13:52 +0200

    DEVIDM-25: Added service end point for CardOrderService

--- src/main/groovy/se/su/it/svc/CardOrderService.groovy
+++ src/main/groovy/se/su/it/svc/CardOrderService.groovy
@@ -1,5 +1,7 @@
 package se.su.it.svc
 
+import se.su.it.svc.commons.SvcAudit
+
 public interface CardOrderService {
-  void findAllCardOrders()
+  void findAllCardOrders(SvcAudit audit)
 }
--- src/main/groovy/se/su/it/svc/CardOrderServiceImpl.groovy
+++ src/main/groovy/se/su/it/svc/CardOrderServiceImpl.groovy
@@ -1,15 +1,17 @@
 package se.su.it.svc
 
 import groovy.util.logging.Slf4j
+import se.su.it.svc.commons.SvcAudit
 import se.su.it.svc.query.SuCardOrderQuery
 
+import javax.jws.WebParam
 import javax.jws.WebService
 
 @WebService @Slf4j
 class CardOrderServiceImpl implements CardOrderService {
 
   @Override
-  void findAllCardOrders() {
+  void findAllCardOrders(@WebParam(name = "audit") SvcAudit audit) {
     SuCardOrderQuery.findAllCardOrders()
   }
 }
