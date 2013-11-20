bec2d75f49d0aabe822f02e78b3b9c19dcc67f60
Joakim Lundin <joakim.lundin@su.se>
2013-11-20 14:41:26 +0100

    Use a proper logger.

--- src/main/groovy/se/su/it/svc/Status.groovy
+++ src/main/groovy/se/su/it/svc/Status.groovy
@@ -31,6 +31,7 @@
 
 package se.su.it.svc
 
+import groovy.util.logging.Slf4j
 import org.springframework.core.io.Resource
 import se.su.it.svc.commons.SvcStatus
 import se.su.it.svc.manager.ApplicationContextProvider
@@ -42,10 +43,11 @@ import javax.jws.WebService
  * This Class handles all Status information for this web service application.
  */
 @WebService
+@Slf4j
 public class Status {
+
   /**
    * Returns a SvcStatus object with status information regarding the web service application and server.
-   *
    *
    * @return an SvcStatus object.
    * @see se.su.it.svc.commons.SvcStatus
@@ -55,21 +57,23 @@ public class Status {
     Resource server   = ApplicationContextProvider.getApplicationContext().getResource("version.properties")
     Properties versionProps = new Properties();
     SvcStatus svcs    = new SvcStatus()
+
     try {
       versionProps.load(me.inputStream)
       svcs.name = versionProps.getProperty("project.name");
       svcs.version = versionProps.getProperty("project.version");
       svcs.buildtime = versionProps.getProperty("project.builddate");
-    } catch (Exception e) {
-      System.out.println("Warning could not load version.properties. " + e.getMessage())
+    } catch (e) {
+      log.warn "Warning could not load version.properties: " + e.getMessage()
     }
+
     try {
       versionProps.load(server.inputStream)
       svcs.sname = versionProps.getProperty("project.name");
       svcs.sversion = versionProps.getProperty("project.version");
       svcs.sbuildtime = versionProps.getProperty("project.builddate");
-    } catch (Exception e) {
-      System.out.println("Warning could not load server version.properties. " + e.getMessage())
+    } catch (e) {
+      log.warn "Warning could not load server version.properties: " + e.getMessage()
     }
 
     return svcs
