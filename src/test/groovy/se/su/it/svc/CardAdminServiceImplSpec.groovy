3ae8a49651c0096567774c675d1e20892d73f4ef
Jan Johansson <janj@su.se>
2016-04-26 16:22:28 +0200

    IDM-1864: Revork unit tests for CardAdminServiceImpl

--- src/test/groovy/se/su/it/svc/CardAdminServiceImplSpec.groovy
+++ src/test/groovy/se/su/it/svc/CardAdminServiceImplSpec.groovy
@@ -8,107 +8,102 @@ import se.su.it.svc.ldap.SuCard
 import se.su.it.svc.query.SuCardOrderQuery
 import se.su.it.svc.query.SuCardQuery
 
+import spock.lang.Shared
 import spock.lang.Specification
 
 class CardAdminServiceImplSpec extends Specification
 {
+    @Shared
+    CardAdminServiceImpl service
 
-  def setup() {
-    GldapoSchemaRegistry.metaClass.add = { Object registration -> }
-    SuCard.metaClass.static.save = {-> }
-    SuCard.metaClass.static.find = { Map arg1, Closure arg2 -> }
-    SuCard.metaClass.static.update = {-> }
-  }
+    def setup()
+    {
+        GldapoSchemaRegistry.metaClass.add = { Object registration -> }
+        SuCard.metaClass.static.save = {-> }
+        SuCard.metaClass.static.find = { Map arg1, Closure arg2 -> }
+        SuCard.metaClass.static.update = {-> }
 
-  def cleanup() {
-    SuCard.metaClass = null
-    SuCardQuery.metaClass = null
-    SuCardOrderQuery.metaClass = null
-    GldapoSchemaRegistry.metaClass = null
-  }
-
-  def "revokeCard with null suCardUUID argument"() {
-    setup:
-    def cardAdminServiceImpl = new CardAdminServiceImpl()
-
-    when:
-    cardAdminServiceImpl.revokeCard(null, 'uid')
-
-    then:
-    thrown(PreconditionViolation)
-  }
-
-  def "revokeCard with null revokerUid argument"() {
-    setup:
-    def cardAdminServiceImpl = new CardAdminServiceImpl()
-
-    when:
-    cardAdminServiceImpl.revokeCard('uuid', null)
-
-    then:
-    thrown(PreconditionViolation)
-  }
-
-  def "revokeCard sets state to revoked"() {
-    setup:
-    def suCard = new SuCard()
-
-    GroovyMock(SuCardQuery, global: true)
-    SuCardQuery.findCardBySuCardUUID(* _) >> { return suCard }
-
-    def cardAdminServiceImpl = new CardAdminServiceImpl()
-
-    cardAdminServiceImpl.suCardOrderQuery = GroovyMock(SuCardOrderQuery) {
-      markCardAsDiscarded(* _) >> { return true }
+        service = new CardAdminServiceImpl()
+        service.suCardOrderQuery = Mock(SuCardOrderQuery)
     }
 
-    when:
-    cardAdminServiceImpl.revokeCard("testcarduuid", 'uid')
-
-    then:
-    suCard.suCardState == "urn:x-su:su-card:state:revoked"
-  }
-
-  def "revokeCard when updating SuCardDb fails"() {
-    setup:
-
-    def suCard = new SuCard()
-    GroovyMock(SuCardQuery, global: true)
-    SuCardQuery.findCardBySuCardUUID(* _) >> { return suCard }
-
-    def cardAdminServiceImpl = new CardAdminServiceImpl()
-
-    cardAdminServiceImpl.suCardOrderQuery = GroovyMock(SuCardOrderQuery) {
-      markCardAsDiscarded(* _) >> { throw new IllegalStateException("foo") }
+    def cleanup()
+    {
+        service = null
+        SuCard.metaClass = null
+        SuCardQuery.metaClass = null
+        SuCardOrderQuery.metaClass = null
+        GldapoSchemaRegistry.metaClass = null
     }
 
-    when:
-    cardAdminServiceImpl.revokeCard("testcarduuid", 'uid')
+    def "revokeCard with null suCardUUID argument"()
+    {
+        when:
+        service.revokeCard(null, 'uid')
 
-    then:
-    thrown(IllegalStateException)
+        then:
+        thrown(PreconditionViolation)
+    }
 
-    and:
-    suCard.suCardState == "urn:x-su:su-card:state:revoked"
-  }
+    def "revokeCard with null revokerUid argument"()
+    {
+        when:
+        service.revokeCard('uuid', null)
 
-  def "revokeCard throws IllegalArgumentException when no card was found"() {
-    setup:
-    SuCardQuery.metaClass.static.findCardBySuCardUUID = { String arg1, String arg2 -> return null }
+        then:
+        thrown(PreconditionViolation)
+    }
 
-    def cardAdminServiceImpl = new CardAdminServiceImpl()
-    when:
-    cardAdminServiceImpl.revokeCard("testcarduuid", 'uid')
-    then:
-    thrown(IllegalArgumentException)
-  }
+    def "revokeCard sets state to revoked"()
+    {
+        setup:
+        def suCard = new SuCard()
+        SuCardQuery.metaClass.static.findCardBySuCardUUID = { String a, String b -> suCard }
 
-  def "setCardPIN is unsupported"() {
-    setup:
-    def cardAdminServiceImpl = new CardAdminServiceImpl()
-    when:
-    cardAdminServiceImpl.setCardPIN(null, "1234")
-    then:
-    thrown(UnsupportedOperationException)
-  }
+        service.suCardOrderQuery.markCardAsDiscarded(*_) >> { return true }
+
+        when:
+        service.revokeCard("testcarduuid", 'uid')
+
+        then:
+        suCard.suCardState == "urn:x-su:su-card:state:revoked"
+    }
+
+    def "revokeCard when updating SuCardDb fails"()
+    {
+        setup:
+        def suCard = new SuCard()
+        SuCardQuery.metaClass.static.findCardBySuCardUUID = { String a, String b -> suCard }
+        service.suCardOrderQuery.markCardAsDiscarded(*_) >> { throw new IllegalStateException("foo") }
+
+        when:
+        service.revokeCard("testcarduuid", 'uid')
+
+        then:
+        thrown(IllegalStateException)
+
+        and:
+        suCard.suCardState == "urn:x-su:su-card:state:revoked"
+    }
+
+    def "revokeCard throws IllegalArgumentException when no card was found"()
+    {
+        setup:
+        SuCardQuery.metaClass.static.findCardBySuCardUUID = { String arg1, String arg2 -> return null }
+
+        when:
+        service.revokeCard("testcarduuid", 'uid')
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "setCardPIN is unsupported"()
+    {
+        when:
+        service.setCardPIN(null, "1234")
+
+        then:
+        thrown(UnsupportedOperationException)
+    }
 }
