5848538f524d9a163e90e4deb14a631504b6b115
Christian Biamont <christian.biamont@su.se>
2018-10-31 10:37:22 +0100

    Remove traces of Sonarqube config,
    Our sonarscanner needs Java 1.8. I tried to workaround this but there
    are missing dependencies upstream to make it work.

--- ci-scripts/pipeline_sukat-cxf-svc.groovy
+++ ci-scripts/pipeline_sukat-cxf-svc.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
 String projectName = env.JOB_NAME
-Boolean isReleaseBuild = false
 
 suSetProperties(["github": "true"])
 
@@ -24,38 +23,12 @@ node('agent')
             suCheckoutCode([projectName: projectName])
         }
 
-        stage("Get information")
-        {
-            def revision = env.rev ?: sh(script: "git log -n 1  --pretty=format:'%H'", returnStdout: true).trim()
-            tag = sh(script: "git tag --contains ${revision} | tail -1", returnStdout: true).trim()
-            if(tag && env.branch == 'master')
-            {
-                isReleaseBuild = true
-            }
-
-        }
-
         suGitHubBuildStatus
         {
 
             stage('Junit Tests')
             {
                 sh './mvnw test'
-            }
-
-            stage('SonarQube analysis')
-            {
-                withSonarQubeEnv('sonarqube')
-                {
-                    if(isReleaseBuild)
-                    {
-                        sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar -Dsonar.projectName=sukat-cxf-svc -Dsonar.branch.name=master'
-                    }
-                    else
-                    {
-                        sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.2:sonar -Dsonar.projectName=sukat-cxf-svc -Dsonar.branch.name=' + env.branch
-                    }
-                }
             }
 
             stage('Compile Code')
